a1d98d439d8412c130852ba723b9103ced2236d6
Ashish <ashish@ubuntu.(none)>
2012-02-01 20:30:14 +0530

    Application created at office Rails 3.2.2+Ruby 1.9.3p125

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kiosk::Application.config.secret_token = 'dbc20a22961d3870a864ece7c548b503f40e1b319e530305487c39a57b49b161428a3da645c3194eb17cda49f0d5a20112ea97f774a2d417ea61f63f1c10513d'
